388d50888c5c370d75083f43c287df1c71ae9f24
hackjustu <wenzhe89929@ucla.edu>
2016-03-15 23:29:04 -0700

    signup page

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,9 @@ var router = require('express').Router();
 var User = require('../models/user');
 
 router.get('/signup', function (req, res, next) {
-	
+	res.render('accounts/signup', {
+		errors: req.flash('errors')
+	});
 });
 
 router.post('/signup', function (req, res, next) {
@@ -11,20 +13,16 @@ router.post('/signup', function (req, res, next) {
 	user.profile.name = req.body.name;
 	user.password = req.body.password;
 	user.email = req.body.email;
-
-//	console.log('name: ' + user.profile.name)
-//	console.log('password: ' + user.password)
-//	console.log('email: ' + user.email)
-
+	
 	User.findOne({ email: req.body.email }, function (err, existingUser) {
 
 		if (existingUser) {
-			console.log(req.body.email + " is already exist.");
+			req.flash('errors', 'Account with that email address already exists.');
 			return res.redirect('/signup');
 		} else {
 			user.save(function (err) {
 				if (err) return next(err);
-				res.json('Successfully created a new user');
+				return res.redirect('/');
 			});
 		}
 	});
